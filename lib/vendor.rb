be1ff01e738c85881a4b9f37c1305e8aa6289830
Amy Holt <amy.elizabeth.holt@gmail.com>
2019-02-17 07:29:50 -0700

    Update market

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -3,25 +3,8 @@ class Vendor
               :inventory
 
   def initialize(name)
-    @inventory = Hash.new(0)
     @name = name
+    @inventory = # you decide what goes here...
   end
 
-  def stock(item, quantity)
-    @inventory[item] += quantity
-  end
-
-  def check_stock(item)
-    @inventory[item]
-  end
-
-  def sell(item, quantity)
-    if check_stock(item) < quantity
-      amount_to_sell = check_stock(item)
-    else
-      amount_to_sell = quantity
-    end
-    @inventory[item] -= amount_to_sell
-    return amount_to_sell
-  end
 end
